dd57c5f9ffb1568da485d42a1cdf5235448da234
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-06-07 14:49:16 -0700

    push in changes

---
+++ AmazonPrer/DataStructures/Node.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace AmazonPrer.DataStructures
+{
+    public class Node
+    {
+        //fields
+        private Node nextNode;
+        private object data;
+
+        //Properties
+        public void setNodeValue(object v1) { data = v1; }
+        public object getNodeValue() { return data; }
+
+        public void setNextNodePointerValue(Node nextNodeAddress) { nextNode = nextNodeAddress; }
+        public object getNextNodePointerValue() { return nextNode; }
+
+        public Node()
+        {
+
+        }
+
+
+    }
+}
--- AmazonPrer/Program.cs
+++ AmazonPrer/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using AmazonPrer.DataStructures;
 
 namespace AmazonPrer
 {
@@ -6,7 +7,24 @@ namespace AmazonPrer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Scratch Pad to test my Data Sctuctures");
+
+            try
+            {
+                Node n1 = new Node();
+                n1.setNodeValue(23);
+                n1.setNextNodePointerValue(null);
+
+                Console.WriteLine(n1.getNodeValue());
+                Console.WriteLine(n1.getNextNodePointerValue());
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+
+
         }
     }
 }
