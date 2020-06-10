ea4d2bde3c3fdd1f51747a59a77196b1ce3523a1
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-06-10 11:32:54 -0700

    added, the insert code, for tree nodes.

--- AmazonPrer/Program.cs
+++ AmazonPrer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using AmazonPrer.DataStructures;
+using AmazonPrer.Recursion;
 
 namespace AmazonPrer
 {
@@ -9,33 +10,84 @@ namespace AmazonPrer
         {
             Console.WriteLine("Scratch Pad to test my Data Sctuctures");
 
-            try
+            int caseswitch = 2;
+            switch(caseswitch)
             {
-                Node n1 = new Node();
-                n1.setNodeValue(23);
-                n1.setNextNodePointerValue(null);
+                case 1:
+                    #region "LinkedList"
+                    try
+                    {
+                        Node n1 = new Node();
+                        n1.setNodeValue(23);
+                        n1.setNextNodePointerValue(null);
 
-                Node n2 = new Node();
-                n2.setNodeValue("Hello");
-                n2.setNextNodePointerValue(null);
+                        Node n2 = new Node();
+                        n2.setNodeValue("Hello");
+                        n2.setNextNodePointerValue(null);
 
-                Node n3 = new Node();
-                n3.setNodeValue(23.45);
-                n3.setNextNodePointerValue(null);
+                        Node n3 = new Node();
+                        n3.setNodeValue(23.45);
+                        n3.setNextNodePointerValue(null);
 
-                list l1 = new list();
-                l1.AddItem(n1);
+                        list l1 = new list();
+                        l1.AddItem(n1);
 
-                l1.AddItem(n2);
-                l1.AddItem(n3);
+                        l1.AddItem(n2);
+                        l1.AddItem(n3);
 
-                l1.printAll();
-                
+                        l1.printAll();
 
-            }
-            catch(Exception ex)
-            {
-                Console.WriteLine(ex);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    #endregion
+                    break;
+
+                case 2:
+                    try
+                    {
+                        //Create a tree, specifying each of the node values manually
+                       // treeNode T1 = new treeNode();
+                       // T1.createTreeNode(23);
+                       // T1.rightchild = new treeNode().createTreeNode(45);
+                       // T1.leftchild = new treeNode().createTreeNode(100);
+                       // Console.WriteLine(T1);
+
+                        //Create a tree from an array of values (no order, generate children by appearance in the array)
+
+                        int[] data = { 1, 2, 3, 4, 5, 6, 7, 8 };
+                        treeNode T2 = new treeNode().GenerateTreeFromArray(data);
+
+
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    break;
+
+                case 3:
+                    try
+                    {
+                     
+                        int[] a = { 1, 2, 3, 4, 5 };
+                        int Total = 0;
+                        Total += recursionPlayground.recurseAddMethod(a, 0);
+                        Console.WriteLine(Total);
+
+                        recursionPlayground.recurseTree(a, 0);
+
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+                    break;
+
+
             }
 
 
---
+++ AmazonPrer/Recursion/recursionPlayground.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace AmazonPrer.Recursion
+{
+    public class recursionPlayground
+    {
+        
+        public recursionPlayground()
+        {
+
+        }
+
+        public static int recurseAddMethod(int[] dataparam, int start)
+        {
+            if (start == dataparam.Length)
+                return 0;
+
+            start++;
+            return recurseAddMethod(dataparam, start) + start;    
+
+        }
+
+        public static void recurseTree(int[] dataparam,int start)
+        {
+            if (start > dataparam.Length)
+                return;
+
+            start++;
+            recurseTree(dataparam, start = 2 * start);
+            Console.WriteLine(start);
+        }
+    }
+}
