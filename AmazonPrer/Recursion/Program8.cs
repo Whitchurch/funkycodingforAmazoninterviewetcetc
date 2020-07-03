fc262996762ccb86d2afac1af1536550b6beee6a
Whitchurch Muthumani <neoblackcyptron@gmail.com>
2020-07-02 19:06:46 -0700

    commit string analysis

---
+++ AmazonPrer/Recursion/Program8.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace coursera
+{
+    class Program8
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            string s = "This is a sti string sti Whitchurch Whitchurch Whitchurch";
+            string[] ssplit = s.Split(" ");
+
+            Dictionary<string, int> words = new Dictionary<string, int>();
+
+            foreach (string item in ssplit)
+            {
+
+                if (words.ContainsKey(item))
+                {
+                    words[item] += 1;
+                }
+                else
+                {
+                    words.Add(item, 1);
+                }
+            }
+
+            int biggest = 0;
+            string maxrepeatedstting = "";
+            foreach (var ite in words)
+            {
+                if (biggest < ite.Value)
+                {
+                    biggest = ite.Value;
+                    maxrepeatedstting = ite.Key;
+                }
+            }
+
+            Console.WriteLine("The most repeated word is {0}, repeated {1}", maxrepeatedstting, biggest);
+        }
+    }
+}
